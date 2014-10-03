7ad3c28370985e9198cd4c890f632ec52c634dc3
gabrielkhofri <izotope115@gmail.com>
2014-10-03 13:36:01 -0700

    adding solution for fizzbuzz advanced.

--- fizz_buzz_adv.rb
+++ fizz_buzz_adv.rb
@@ -1,7 +1,29 @@
 
-1.upto(100) do |n|
-  print "Fizz" if a = (n % 3).zero?
-  print "Buzz" if b = (n % 5).zero?
-  print n unless (a || b)
-  puts
+INPUT_RANGE = (1..105)
+WORDS_HASH = { 3 => "Fizz",
+               5 => "Buzz",
+               7 => "Sivv",
+               13 => "Grr"
+}
+
+class FizzBuzzSivv
+
+  def evaluate(number)
+    @output = ""
+    WORDS_HASH.each do |key, value|
+      if number % key == 0
+        @output += value
+      end
+    end
+    @output.empty? ? number : @output
+  end
+
+end
+
+fbs = FizzBuzzSivv.new
+puts "Your translation specs are: #{WORDS_HASH}"
+puts "Your range is: #{INPUT_RANGE.min} to #{INPUT_RANGE.max}"
+
+INPUT_RANGE.each do |number|
+  puts fbs.evaluate(number)
 end
